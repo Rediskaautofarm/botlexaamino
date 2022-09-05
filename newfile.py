30e5b95e5610b66559839045db96d78319f7caf4
Rediskaautofarm <110743861+Rediskaautofarm@users.noreply.github.com>
2022-09-05 17:51:37 +0300

    Add files via upload

--- newfile.py
+++ newfile.py
@@ -1,18 +1,20 @@
 from edamino import Bot, Context, logger
 from edamino.objects import UserProfile
 from edamino.api import File
+import random
 
 bot = Bot(email='ment---3zolJcUB9Hxb@wwjmp.com', password='!zetking1488', prefix="")
 
 
+hellotxt = ['привет!', 'че как?', 'хай', 'здарова']
 @bot.event()
 async def on_ready(profile: UserProfile):
     logger.info(f'{profile.nickname} ready')
     
 
-@bot.command('привет', 'хай', 'пр')
+@bot.command('привет')
 async def echo(ctx: Context):
-    await ctx.reply('Pong!')
+    await ctx.reply(random.choice(hellotxt))
     
 @bot.command('image')
 async def on_image(ctx: Context):
